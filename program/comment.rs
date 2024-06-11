0d10c482a69ad1727591f837a6330ea0d6f592e2
Santosh Kumar Panda <40318059+santoshpanda1995@users.noreply.github.com>
2024-06-11 09:49:43 +0530

    Update comment.rs

--- program/comment.rs
+++ program/comment.rs
@@ -1,7 +1,8 @@
 fn main() {
     // single line comment
-    let some_number = 100; // Write the comment here
+    let n = 100; // Write the comment here
     /*
     Multi-line comment
     */
+    println!("number is {}",n)
 }
