8bda4d999d931d36e8c6552b9c5e1402c132e24d
Santosh Kumar Panda <40318059+santoshpanda1995@users.noreply.github.com>
2024-06-11 10:31:28 +0530

    Update integers.rs

--- program/integers.rs
+++ program/integers.rs
@@ -4,4 +4,14 @@ fn main() {
    let sum:i32=5-15;
    println!("result is {}",result);
    println!("sum is {} and age is {}", sum,age);
+
+   let small_number1 = 10u8;
+   println!("small number is {}",small_number1);
+   let small_number2 = 10_u8; // This is easier to read
+   let big_number = 100_000_000_i32; // 100 million is easy to read with _
+   println!("{}, {}", small_number2, big_number);
+   
+   let number1 = 0________u8;
+   let number2 = 1___6______2____4______i32;
+   println!("{}, {}", number1, number2);
 }
