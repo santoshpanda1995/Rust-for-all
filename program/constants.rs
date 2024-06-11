24fbb419e6c0222520d07dbf2b56d0e95b405803
Santosh Kumar Panda <40318059+santoshpanda1995@users.noreply.github.com>
2024-06-11 11:06:27 +0530

    Create constants.rs

---
+++ program/constants.rs
@@ -0,0 +1,7 @@
+fn main(){
+  // Constants represents values that cannot be changed. If you declare
+  // a constant then there is no way its value changes.
+  const PI:f32=3.14; // declare float constant
+  const NUM: i32=100; // declare integer constant
+  println!("{},{}",PI,NUM);
+}
