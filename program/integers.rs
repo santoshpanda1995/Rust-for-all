0eabadb49ea1686fbf0866589c08a76146fb3cef
Santosh Kumar Panda <40318059+santoshpanda1995@users.noreply.github.com>
2024-06-11 10:49:15 +0530

    Update integers.rs

--- program/integers.rs
+++ program/integers.rs
@@ -7,11 +7,4 @@ fn main() {
 
    let small_number1 = 10u8;
    println!("small number is {}",small_number1);
-   let small_number2 = 10_u8; // This is easier to read
-   let big_number = 100_000_000_i32; // 100 million is easy to read with _
-   println!("{}, {}", small_number2, big_number);
-   
-   let number1 = 0________u8;
-   let number2 = 1___6______2____4______i32;
-   println!("{}, {}", number1, number2);
 }
