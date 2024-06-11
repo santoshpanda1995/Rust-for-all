ee967575e5a3cb0ad6a118458e1a290d06d08379
Santosh Kumar Panda <40318059+santoshpanda1995@users.noreply.github.com>
2024-06-11 10:47:11 +0530

    Update integerrange.rs

--- program/integerrange.rs
+++ program/integerrange.rs
@@ -1,4 +1,12 @@
 fn main(){
-  let age:u8=255;
-  let weight:u8=256; // Error as u8 range is 0 to 255
+    println!("The smallest i8 is {} and the biggest i8 is {}.", i8::MIN, i8::MAX); 
+    println!("The smallest u8 is {} and the biggest u8 is {}.", u8::MIN, u8::MAX);
+    println!("The smallest i16 is {} and the biggest i16 is {}.", i16::MIN, i16::MAX);
+    println!("The smallest u16 is {} and the biggest u16 is {}.", u16::MIN, u16::MAX);
+    println!("The smallest i32 is {} and the biggest i32 is {}.", i32::MIN, i32::MAX);
+    println!("The smallest u32 is {} and the biggest u32 is {}.", u32::MIN, u32::MAX);
+    println!("The smallest i64 is {} and the biggest i64 is {}.", i64::MIN, i64::MAX);
+    println!("The smallest u64 is {} and the biggest u64 is {}.", u64::MIN, u64::MAX);
+    println!("The smallest i128 is {} and the biggest i128 is {}.", i128::MIN, i128::MAX);
+    println!("The smallest u128 is {} and the biggest u128 is {}.", u128::MIN, u128::MAX);
 }
